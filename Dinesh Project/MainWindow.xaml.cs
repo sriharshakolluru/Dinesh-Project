0c7d08b6f1930beabfc3ec655877144e40fca793
sriharsha <sriharshakolluru@in.com>
2013-12-10 23:46:46 +0530

    After Initial Tab Addings

--- Dinesh Project/MainWindow.xaml.cs
+++ Dinesh Project/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace Dinesh_Project
 {
@@ -26,12 +27,53 @@ namespace Dinesh_Project
 
         private void Page1_Click(object sender, RoutedEventArgs e)
         {
-            tabControlView.Items.Add(new Transactions());
+            TabItem tabitem = new TabItem();
+            tabitem.Header = "Transactions";
+            Frame tabFrame = new Frame();
+            Transactions page1 = new Transactions();
+            tabFrame.Content = page1;
+            tabitem.Content = tabFrame;
+            tabitem.Name = "Transactions";
+            if (AllowTab(tabitem.Name))
+                tabControlView.Items.Add(tabitem);
+            else
+                MessageBox.Show("Tab is Already Open or Too Many Tabs", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
         private void Page2_Click(object sender, RoutedEventArgs e)
         {
-            tabControlView.Items.Add(new  Transactions());
+
+            TabItem tabitem = new TabItem();
+            tabitem.Header = "Reports";
+            Frame tabFrame = new Frame();
+            Reports page1 = new Reports();
+            tabFrame.Content = page1;
+            tabitem.Content = tabFrame;
+            tabitem.Name = "Reports";
+            if (AllowTab(tabitem.Name))
+                tabControlView.Items.Add(tabitem);
+            else
+                MessageBox.Show("Tab is Already Open or Too Many Tabs", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+        public bool AllowTab(string Name)
+        {
+            var activeTabs = (from TabItem item in tabControlView.Items
+                              select item.Name);
+            if (activeTabs.Contains(Name))
+                return false;
+            else if (activeTabs.Count() > 5)
+                return false;
+            else
+                return true;
+        }
+
+        private void Page3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabControlClicked(object sender, MouseButtonEventArgs e)
+        {
             
         }
     }
