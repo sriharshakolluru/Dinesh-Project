343fbda0acfd3b5edbb9c35b26006182b58ed579
sriharsha <sriharshakolluru@in.com>
2013-12-12 00:25:01 +0530

    Inter

---
+++ DataOperations/CoreOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace DataOperations
+{
+    public class CoreOperations
+    {
+        ConnectionStringSettings coreConnectionstring = ConfigurationManager.ConnectionStrings["CoreDbConnectionString"];
+
+        public DataSet GetData(ConnectionStringSettings connection, string command)
+        {
+            SqlConnection currentConnection = new SqlConnection(coreConnectionstring.ToString());
+            SqlCommand cmd= currentConnection.CreateCommand();
+            cmd.CommandText = command;
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataSet resultset = new DataSet();
+            return resultset;
+        }
+
+
+
+    }
+}
