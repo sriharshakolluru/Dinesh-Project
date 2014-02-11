aa7b6c2e31aa97ab3be155561c42e7d67d8f73dc
sriharshakolluru <sriharshakolluru2007@gmail.com>
2014-02-11 16:50:28 +0530

    Password Dialogue

---
+++ Dinesh Project/LoginPopup.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dinesh_Project
+{
+    /// <summary>
+    /// Interaction logic for LoginPopup.xaml
+    /// </summary>
+    public partial class LoginPopup : Window
+    {
+        public LoginPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void loginId_keyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key.Equals(Key.Enter))
+            {
+                txtPassword.Focus();
+            }
+        }
+
+        private void key_down(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                isAuthenticationPassed();
+                
+            }
+        }
+        private bool isAuthenticationPassed()
+        {
+            string userName = txtLoginId.Text;
+            string password=txtPassword.Password;
+
+            return false;
+        }
+    }
+}
