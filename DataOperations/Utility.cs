59461e0f3966c787aa9ccbd39287b9d439ad424f
sriharshakolluru <sriharshakolluru2007@gmail.com>
2014-02-12 10:51:11 +0530

    Temp Commit

--- DataOperations/Utility.cs
+++ DataOperations/Utility.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
 using System.IO;
+using System.Security.Cryptography;
 namespace DataOperations
 {
    public class Utility
@@ -95,6 +96,59 @@ namespace DataOperations
             }
             return false;
         }
+
+        /// <summary>
+        /// take any string and encrypt it using MD5 then
+        /// return the encrypted data 
+        /// </summary>
+        /// <param name="data">input text you will enterd to encrypt it</param>
+        /// <returns>return the encrypted text as hexadecimal string</returns>
+        private string GetMD5HashData(string data)
+        {
+            //create new instance of md5
+            MD5 md5 = MD5.Create();
+
+            //convert the input text to array of bytes
+            byte[] hashData = md5.ComputeHash(Encoding.Default.GetBytes(data));
+
+            //create new instance of StringBuilder to save hashed data
+            StringBuilder returnValue = new StringBuilder();
+
+            //loop for each byte and add it to StringBuilder
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString());
+            }
+
+            // return hexadecimal string
+            return returnValue.ToString();
+
+        }
+
+        /// <summary>
+        /// encrypt input text using MD5 and compare it with
+        /// the stored encrypted text
+        /// </summary>
+        /// <param name="inputData">input text you will enterd to encrypt it</param>
+        /// <param name="storedHashData">the encrypted text
+        ///         stored on file or database ... etc</param>
+        /// <returns>true or false depending on input validation</returns>
+        private bool ValidateMD5HashData(string inputData, string storedHashData)
+        {
+            //hash input text and save it string variable
+            string getHashInputData = GetMD5HashData(inputData);
+
+            if (string.Compare(getHashInputData, storedHashData) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+       
 
     }
 }
